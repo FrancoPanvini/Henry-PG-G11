c6182096c7a01d611a1de8c4277644b1ec57073e
damldieg <damian.diego.2510@gmail.com>
2021-09-27 16:07:23 +0200

    feat: client add tailwind and normalize

---
+++ Client/my-app/src/App/reducer/index.js
@@ -0,0 +1,12 @@
+//import prueba from './debug'
+
+const initialState = {
+    prueba : []
+  };
+
+
+function rootReducer(state = initialState, action) {
+    return state;
+}
+  
+export default rootReducer;
