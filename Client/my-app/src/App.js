ae47352a60639f4ad2b609c1af19529247a63df0
damldieg <damian.diego.2510@gmail.com>
2021-09-28 16:53:52 +0200

    feat: set home

--- Client/my-app/src/App.js
+++ Client/my-app/src/App.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import './App.css';
 import {BrowserRouter as Router} from 'react-router-dom';
+import Navbar from './App/components/Navbar';
+import Footer from './App/components/Footer';
+import Home from './App/components/Home';
 //import Normalize from 'react-normalize';
 
 function App() {
   return (
     <Router>
       {/* <Normalize /> */}
-      <div className="bg-gray-200 text-center">
-        <h1>A<span className="text-purple-400">dog</span>tame</h1>
-      </div>
+      <Navbar/>
+        <Home/>
+      <Footer/>
     </Router>
   );
 }
