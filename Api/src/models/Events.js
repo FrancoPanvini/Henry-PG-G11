e9992671ccdd26b095b310df9dd6ed59aa669439
Franco Panvini <francopanvini@gmail.com>
2021-09-28 13:15:54 -0300

    update-back models Event/User/Pets format

--- Api/src/models/Events.js
+++ Api/src/models/Events.js
@@ -6,18 +6,13 @@ module.exports = sequelize => {
     {
       name: { type: DataTypes.STRING, allowNull: false },
       description: { type: DataTypes.STRING },
-      //* Agregar una imagen de not available en cloudinary y poner esa URL
-      photo: {
-        type: DataTypes.STRING /*,defaultValue='https://www.ecpgr.cgiar.org/fileadmin/templates/ecpgr.org/Assets/images/No_Image_Available.jpg'*/,
-      },
+      photo: { type: DataTypes.STRING },
       initDate: { type: DataTypes.DATE, allowNull: false },
       endDate: { type: DataTypes.DATE, allowNull: false },
-      location: { type: DataTypes.STRING, allowNull: false },
       direction: { type: DataTypes.STRING, allowNull: false },
     },
     { sequelize: sequelize, modelName: "Events", timestamps: true, paranoid: true }
   );
-  //* guardar todo en lowercase les copa?
   Events.beforeCreate(function (event) {
     event.name = event.name.toLowerCase();
     event.direction = event.direction.toLowerCase();
--- Api/src/models/Pets.js
+++ Api/src/models/Pets.js
@@ -1,25 +1,21 @@
-const { Model, DataTypes } = require('sequelize');
-
+const { Model, DataTypes } = require("sequelize");
 
 module.exports = sequelize => {
   class Pets extends Model {}
   Pets.init(
     {
       name: { type: DataTypes.STRING, allowNull: false },
-      sex: { type: DataTypes.STRING, allowNull: true },
-      age: { type: DataTypes.STRING, allowNull: true },
-      size: { type: DataTypes.STRING, allowNull: true },
-      description: { type: DataTypes.STRING, allowNull: true },
+      size: { type: DataTypes.STRING, allowNull: true, validate: { isIn: ["g", "m", "c"] } },
+      sex: { type: DataTypes.STRING, allowNull: true, validate: { isIn: ["m", "h"] } },
+      age: { type: DataTypes.INTEGER, allowNull: true },
+      description: { type: DataTypes.TEXT, allowNull: true },
       adopted: { type: DataTypes.BOOLEAN, allowNull: false },
     },
     { sequelize: sequelize, modelName: "Pets", timestamps: true, paranoid: true }
-  )
+  );
 
   Pets.beforeCreate(function (event) {
     event.name = event.name.toLowerCase();
-    event.age = event.age.toLowerCase();
-    event.description = event.description.toLowerCase();
     return event;
   });
-  }
-
+};
