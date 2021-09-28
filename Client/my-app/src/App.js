613ebe9f3a42a59df93384e1d732a597184329de
damldieg <damian.diego.2510@gmail.com>
2021-09-28 16:46:30 +0200

    Co-authored-by: davidmenlop <davidmenlop@users.noreply.github.com>

--- Client/my-app/src/App.js
+++ Client/my-app/src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './App.css';
 import {BrowserRouter as Router} from 'react-router-dom';
-import Normalize from 'react-normalize';
+//import Normalize from 'react-normalize';
 
 function App() {
   return (
     <Router>
-      <Normalize/>
+      {/* <Normalize /> */}
       <div className="bg-gray-200 text-center">
         <h1>A<span className="text-purple-400">dog</span>tame</h1>
       </div>
