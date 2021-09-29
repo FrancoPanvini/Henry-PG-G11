b26402573b54c67322e71cc2a4c75de7ad5a7501
LucasChaves12 <84579422+LucasChaves12@users.noreply.github.com>
2021-09-29 09:47:20 -0300

    fix-back-eventRoute fixes

--- Api/src/routes/sub-routes/controllers/events_controllers/getEventbyId.js
+++ Api/src/routes/sub-routes/controllers/events_controllers/getEventbyId.js
@@ -1,11 +1,9 @@
-const { Events} = require("../../../../db")
+const { Events } = require("../../../../db");
 
 const getEventById = async (req, res) => {
-    const { id } = req.params
-    console.log(id)
-       let eventid = await Events.findAll({where: {id: id}})
-       res.status(200).json(eventid)
-    
-}
+  const { id } = req.params;
+  let eventid = await Events.findAll({ where: { id: id } });
+  res.status(200).json(eventid);
+};
 
-module.exports = getEventById
+module.exports = getEventById;
--- Api/src/routes/sub-routes/controllers/events_controllers/postEvents.js
+++ Api/src/routes/sub-routes/controllers/events_controllers/postEvents.js
@@ -1,31 +1,21 @@
-const { Users, Events, Cities} = require("../../../../db")
+const { Events } = require("../../../../db");
 
-const postEvents = async (req, res) =>{
+const postEvents = async (req, res) => {
+  const { name, description, initDate, endDate, direction, cityId, UserId } = req.body;
+  try {
+    let eventCreated = await Events.create({
+      name,
+      description,
+      initDate,
+      endDate,
+      direction,
+    });
+    await eventCreated.setUser(req.body.UserId);
+    await eventCreated.setCity(cityId);
+    res.status(200).send("UserId");
+  } catch (err) {
+    res.status(409).send(err);
+  }
+};
 
-    const {
-        name,
-        description,
-        initDate,
-        endDate,
-        direction,
-        cityId,
-        UserId,
-    } = req.body
-    console.log(UserId)
-    try{
-        let eventCreated = await Events.create({
-            name,
-            description,
-            initDate,
-            endDate,
-            direction
-        })
-        await eventCreated.setUser(req.body.UserId)
-        await eventCreated.setCity(cityId)
-        res.status(200).send('UserId')
-    } catch (err) {
-        console.error(err)
-    }
-}
-
-module.exports = postEvents
+module.exports = postEvents;
