70881c492a8a2f6483e082e6a4d4e26f6e6cf017
damldieg <damian.diego.2510@gmail.com>
2021-09-29 14:32:28 +0200

    Fix: Footer

--- Client/my-app/src/App/components/Footer.jsx
+++ Client/my-app/src/App/components/Footer.jsx
@@ -6,7 +6,7 @@ function Footer() {
     return (
         <div className="flex justify-between px-4 bg-primary items-center text-white">
             <NavLink to="/" className="text-xl flex items-center">AD<FaPaw/>GTAME</NavLink>
-            <p className="text-left w-4/5 ml-4">© 2021 Adogtame — Todos los derechos reservados</p>
+            <p className="text-left w-4/5 ml-4">© 2021 Adogtame — Todos los derechos reservados.</p>
           <div className="flex  text-center items-center ">
             <NavLink to="/adopciones" className="p-4 text-center focus:text-tercero hover:text-tercero">NOSOTROS</NavLink>
             <NavLink to="/adopciones" className="p-4  text-center focus:text-tercero hover:text-tercero">FAQ</NavLink>  
