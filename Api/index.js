88a789d15832c2765802b83556ff016fc8b679ca
Franco Panvini <francopanvini@gmail.com>
2021-09-29 14:02:54 -0300

    feat-back add filters getPets

--- Api/index.js
+++ Api/index.js
@@ -1,11 +1,40 @@
 const server = require("./src/app.js");
-const { conn } = require("./src/db.js");
+const { conn, Pets, PetsType, UsersType, Users } = require("./src/db.js");
 
 //* Descomentar esta línea para restartear la DB
 /* conn.sync({ force: true }).then(() => { */
 //* Descomentar esta línea para NO restartear la DB
-conn.sync().then(() => {
-  server.listen(3001, () => {
-    console.log("Server listening at 3001");
+conn
+  .sync()
+  .then(() => {
+    server.listen(3001, () => {
+      console.log("Server listening at 3001");
+    });
+  })
+  .then(() => {
+    // PetsType.bulkCreate(petsTypesArray);
+    // UsersType.bulkCreate(usersTypesArray);
+    // Pets.bulkCreate(petsArray);
+    // Users.bulkCreate(userArray);
   });
-});
+
+const petsTypesArray = [
+  { id: "p", type: "Perro" },
+  { id: "g", type: "Gato" },
+];
+const usersTypesArray = [
+  { id: "i", type: "Individuo" },
+  { id: "r", type: "Refugio" },
+  { id: "a", type: "Admin" },
+];
+const userArray = [
+  { name: "Franco Panvini", mail: "franco@gmail.com", phone: "5434195548", direction: "maipu 516", password: "123456", dni: 33807938 },
+  { name: "lucas chaves", mail: "lucas@gmail.com", phone: "5434198576", direction: "1234 calle", password: "dfasfsadg", dni: 35248 },
+  { name: "santiago petri", mail: "santi@hotmail.com", phone: "5489571556", direction: "2587asd asd", password: "sdafsa asfgas", dni: 54796315 },
+];
+const petsArray = [
+  { name: "Juancho", PetsTypeId: "p", CityId: 1, sex: "m", size: "c", age: 0, Ownerid: 1 },
+  { name: "Juana", PetsTypeId: "g", CityId: 2, sex: "h", size: "m", age: 2, Ownerid: 2 },
+  { name: "Renato", PetsTypeId: "p", CityId: 2, sex: "m", size: "g", age: 3, Ownerid: 3 },
+  { name: "Pipita", PetsTypeId: "g", CityId: 3, sex: "h", size: "g", age: 8, Ownerid: 2 },
+];
--- Api/src/routes/sub-routes/pets.js
+++ Api/src/routes/sub-routes/pets.js
@@ -1,15 +1,16 @@
 const express = require("express");
 const router = express.Router();
 const getPets = require("./controllers/pets_controllers/getPets");
+// const postPet = require("./controllers/pets_controllers/postPet");
 
 
 //ver todos los animales (con filters)
-
-router.get("/all", getPets);
+router.get("/", getPets);
 
 //Ver detalle de una mascota
 
-//Publicar una nueva mascota
+//Post una nueva mascota
+// router.get("/all", postPet);
 
 //Modificar una mascota
 
