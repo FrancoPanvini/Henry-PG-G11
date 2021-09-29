d47883f68fc8590078e2805c2f1b627988ccc8b6
davidmenlop <davidmenlop@gmail.com>
2021-09-29 11:46:45 -0500

    Animacion de la Card

--- Client/my-app/src/App/components/Card.jsx
+++ Client/my-app/src/App/components/Card.jsx
@@ -1,10 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
+import ReactCardFlip from "react-card-flip";
 
-
-function Card({photo}) {
+function Card({ photo }) {
   //var photo = "https://www.hola.com/imagenes/estar-bien/20201105178590/perro-maltipoo-raza-pequena/0-937-760/maltipoo-perro-t.jpg";
 
+  const [isFlipped, setIsFlipped] = useState(false);
+
+  const handleClick = () => {
+    setIsFlipped(!isFlipped);
+  };
+
   return (
+    <div >
+      <ReactCardFlip isFlipped={isFlipped} flipDirection="horizontal">
+        <div className="card">
+          <img src={photo} alt="" className="w-full" onClick={handleClick}/>
+          <h3 className='grid justify-items-center font-bold text-white bg-primary'>Nombre Mascota</h3>
+        </div>
+
+        <div>
+          <div className="card px-6 py-4 h-full w-full bg-secondary" onClick={handleClick}>
+            <div className="font-bold text-xl mb-2">Nombre Mascota</div>
+            <p className="text-gray-700 text-xs">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              Voluptatibus quia, nulla! Maiores et perferendis eaque,
+              exercitationem praesentium nihil.
+              <br />
+              <br />
+              <button className="btn bg-yellow-600 text-white border-yellow-700">
+                Contactame
+              </button>
+            </p>
+          </div>
+        </div>
+      </ReactCardFlip>
+    </div>
+  );
+
+  /* return (
     <div className="card">
       <img src={photo} alt="" className="w-full" />
       <div className="px-6 py-4">
@@ -15,7 +48,7 @@ function Card({photo}) {
           nihil.
         </p>
       </div>
-      <div className="px-6 py-4 bg-red-400">
+      <div className="px-6 py-4 bg-primary">
         <span className="inline-block bg-gray-200 rounded-full px-1 py-1 text-xs font-semibold text-gray-700 mr-2">
           Buenos Aires
         </span>
@@ -27,7 +60,7 @@ function Card({photo}) {
         </span>
       </div>
     </div>
-  );
+  ); */
 }
 
 export default Card;
--- Client/my-app/src/App/components/Home.jsx
+++ Client/my-app/src/App/components/Home.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ContenedorCard from './ContenedorCard'
+import Onboarding from './Onboarding'
 //import './Card.module.css'
 
 function Home() {
     return (
         <div>
+            <Onboarding/>
+            <br />
             <ContenedorCard title='ADOPCION'/>
             <br />
             <ContenedorCard title='EXTRAVIADOS'/>
