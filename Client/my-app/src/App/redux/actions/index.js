c3a04de0ca71d1535e8dfdff5dc7ed86423a31d3
damldieg <damian.diego.2510@gmail.com>
2021-09-30 21:53:37 +0200

    feat: actions and install axios

---
+++ Client/my-app/src/App/redux/actions/index.js
@@ -0,0 +1,21 @@
+import axios from 'axios';
+
+export const getPetsAdop = () => {
+    
+}
+
+export const getPetsPerd = () => {
+
+}
+
+export const getUser = () => {
+
+}
+
+export const getEvents = () => {
+
+}
+
+export const getAllPets = () => {
+
+}
