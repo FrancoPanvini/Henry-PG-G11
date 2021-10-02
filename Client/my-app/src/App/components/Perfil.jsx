b36cf0225538c21fb125231f97c518ccf4dd7345
ffranco-a <84511984+ffranco-a@users.noreply.github.com>
2021-10-02 18:31:47 -0300

    fixed profile

--- Client/my-app/src/App/components/Perfil.jsx
+++ Client/my-app/src/App/components/Perfil.jsx
@@ -1,60 +1,50 @@
 import React from 'react';
 import axios from 'axios';
 import { useState, useEffect } from 'react';
+import { FaPaw } from 'react-icons/fa';
 
 function Perfil() {
   const userId = localStorage.getItem('userId');
-  const [user, setUser] = useState({});
-  const [url, setUrl] = useState('');
+  const [user, setUser] = useState(null);
+  const [url, setUrl] = useState(''); // ← ¿para qué se usa esto?
 
   const initialUser = () => {
     axios
       .get(`users/${userId}`)
       .then(res => {
         console.log(res);
-        setUser(res.data[0]);
+        setUser(res.data);
         return res.data;
       })
-      .then(res => setUrl(res.image))
+      .then(res => setUrl(res.image)) // ← ¿qué es esto de image?
       .catch(err => console.log(err));
   };
-  useEffect(initialUser, [userId]);
-
-  /* ↓ DELETE ↓ */
-  if (user === undefined) {
-    setUser({
-      name: 'hardcodeado, comentar líneas 24-34',
-      mail: 'mail@mail.com',
-      phone: 123456789,
-      direction: 'Calle falsa 123',
-    });
-  }
-  if (url === undefined) {
-    setUrl(
-      'https://image.shutterstock.com/image-photo/closeup-young-beautiful-woman-taking-600w-402737050.jpg'
-    );
-  }
-  /* ↑ DELETE ↑ */
+  useEffect(initialUser, [userId, user]);
 
   return (
     <div className="bg-gradient-to-r from-thirty to-fourty flex justify-center items-center h-screen85">
       <div className="text-center text-white text-lg p-8 w-2/5 min-w-max rounded-lg bg-gradient-to-l from-secondaryDark to-thirty border-2 border-white border-opacity-20">
-
         {/* ↓↓↓ DELETE ... o restructurar ↓↓↓ */}
-        {user === undefined ? (
-          <div> Loading... </div>
+        {user === null ? (
+          <> Loading... </>
         ) : (
           <>
-        {/* ↑↑↑ DELETE ... o restructurar ↑↑↑ */}
+            {/* ↑↑↑ DELETE ... o restructurar ↑↑↑ */}
 
-            <div className="flex gap-4 justify-center">
-              <div className="bg-fourty h-32 w-32 rounded-lg overflow-hidden shadow-similBorderWhite">
-                <img
-                  src={url}
-                  alt="foto"
-                  className="z-0 h-full w-full object-cover"
-                />
+            <div className="flex justify-center items-center">
+              <div className="mr-8 flex justify-center items-center bg-fourty w-20 h-20 rounded-full">
+                <FaPaw className="text-white text-3xl" />
               </div>
+
+              {/* ↓ si los perfiles tienen foto, usar este div para ello */}
+              {/* <div className="bg-fourty h-32 w-32 rounded-lg overflow-hidden shadow-similBorderWhite">
+                  <img
+                    src={url}
+                    alt="foto"
+                    className="z-0 h-full w-full object-cover"
+                  />
+                </div> */}
+
               <div>
                 <div>¡Hola {user.name}!</div>
                 <div>Tu email es {user.mail}</div>
@@ -66,10 +56,16 @@ function Perfil() {
             <br />
 
             {/* ↓ botones que para la primer demo estarán invisibles */}
-            <div /* className="invisible" */ >
-              <button className="btn btn-nav mx-2 px-4 bg-primaryDark">Mis publicaciones</button>
-              <button className="btn btn-nav mx-2 px-4 bg-primaryDark">Cerrar sesión</button>
-              <button className="btn btn-nav mx-2 px-4 bg-primaryDark">Borrar mi cuenta</button>
+            <div /* className="invisible" */>
+              <button className="btn btn-nav mx-2 px-4 bg-primaryDark">
+                Mis publicaciones
+              </button>
+              <button className="btn btn-nav mx-2 px-4 bg-primaryDark">
+                Cerrar sesión
+              </button>
+              <button className="btn btn-nav mx-2 px-4 bg-primaryDark">
+                Borrar mi cuenta
+              </button>
             </div>
 
           {/* ↓ si se borra lo de la línea 41-45, borrar esto tmb  */}
