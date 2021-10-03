b54a35d742c05e8ad55035ba803ded631c143765
davidmenlop <davidmenlop@gmail.com>
2021-10-03 07:51:53 -0500

    Pruebas mantener sesion iniciada

--- Client/my-app/src/App/components/Adopciones.jsx
+++ Client/my-app/src/App/components/Adopciones.jsx
@@ -1,23 +1,27 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import ContainerCardAdopcion from './ContainerCardAdopcion';
 import FiltersBar from './FiltersBar';
 import { Link } from 'react-router-dom';
 
 
 function Adopciones() {
+    const isLogged = useSelector((state) => state.isLogged);
+
     return (
     <div>
         <div className='flex justify-between items-center bg-gray-200'>
             <FiltersBar/>
             <ContainerCardAdopcion title="ADOPCIONES"/>
         </div>
-        <div className="flex justify-center py-12 bg-gray-200">
+        {isLogged?(<div className="flex justify-center py-12 bg-gray-200">
             <Link to="/adopciones/ofrecer" className="mx-auto" >
                 <button className="btn btn-lg bg-thirty text-white border-fourty">
                     Ofrecer una mascota en adopción
                 </button>
             </Link>
-        </div>
+        </div>) : null}
+        
     </div>
     )
 }
