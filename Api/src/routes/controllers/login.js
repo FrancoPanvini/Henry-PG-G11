cd31d204e43eac0ccc0da22f21ec7a52d52bf8a3
Franco Panvini <francopanvini@gmail.com>
2021-10-04 09:04:56 -0300

    Fix-back format login Route & service

--- Api/src/routes/controllers/login.js
+++ Api/src/routes/controllers/login.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
-const postLogin = require("./services/login_services/postlogin")
+const postLogin = require("./services/login_services/postlogin");
 
-router.post("/", postLogin)
+router.post("/", postLogin);
 
-module.exports = router
+module.exports = router;
