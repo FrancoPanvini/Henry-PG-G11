4e040f72aff6995766e4da6178ad9d04f2e8411c
Franco Panvini <francopanvini@gmail.com>
2021-10-04 09:01:37 -0300

    feat-back add getLostPets service & route

--- Api/src/routes/controllers/lost_pets.js
+++ Api/src/routes/controllers/lost_pets.js
@@ -1,37 +1,16 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-/* const { } = require('./services/pets_controllers'); */
+const getLostPets = require("./services/lost_pets_services/getLostPets");
 
-
-//ver todos los animales (con filters(zona,fecha))
-
-
+//ver todos los animales perdidos
+router.get("/", getLostPets);
 
 //Ver detalle de una mascota
 
-
-
 //Publicar una nueva mascota
-
-
 
 //Modificar una mascota(datos/perdida-encontrada)
 
-
-
 //Eliminar una mascota
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-module.exports = router
+module.exports = router;
