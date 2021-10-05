64ba1f5cb599bd34292ec0675bfe75286dc3f9f4
FrancoPanvini <84184581+FrancoPanvini@users.noreply.github.com>
2021-10-05 16:16:22 -0300

    Update actions /users en postUsers

--- Client/my-app/src/App/redux/actions/index.js
+++ Client/my-app/src/App/redux/actions/index.js
@@ -88,9 +88,8 @@ export const getAllPets = () => {
 
 
 export function postUsers(payload) {
-    // console.log(payload)
      return async function (dispatch){
-         const response = await axios.post("users/", payload)
+         const response = await axios.post("/users", payload)
          console.log(response);
          return response;
      }
@@ -115,7 +114,6 @@ export function setUser(user) {
     }
 }
 export function logInUsers(payload) {
-    // console.log(payload)
      return async function (dispatch){
          const response = await axios.post("login/", payload).then((res) => {
             if(res.data.message) {
@@ -130,7 +128,6 @@ export function logInUsers(payload) {
           }).catch ((err) => {
             alert(`${err}`)
           })
-         console.log(response);
          return response;
      }
      
