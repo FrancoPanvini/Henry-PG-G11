823c7360cda55379b647e65c337055db62d3fb9d
Franco Panvini <francopanvini@gmail.com>
2021-10-06 08:17:02 -0300

    update-front card adopción styles

--- Client/my-app/src/App/components/Cards/CardAdopcion.jsx
+++ Client/my-app/src/App/components/Cards/CardAdopcion.jsx
@@ -1,12 +1,13 @@
-import React, { useState } from 'react';
-import { useSelector } from 'react-redux';
-import ReactCardFlip from 'react-card-flip';
-import Modal from '../pop-up/modal';
+import React, { useState } from "react";
+import { useSelector } from "react-redux";
+import ReactCardFlip from "react-card-flip";
+import Modal from "../pop-up/modal";
+import { FaPaw } from "react-icons/fa";
 
 function CardAdopcion({ photo, name, age, size, country, province, city, sex, description }) {
   const [isFlipped, setIsFlipped] = useState(false);
   const [isOpen, setIsOpen] = useState(false);
-  const isLogged = useSelector((state) => state.isLogged);
+  const isLogged = useSelector(state => state.isLogged);
 
   const handleClick = () => {
     setIsFlipped(!isFlipped);
@@ -22,10 +23,7 @@ function CardAdopcion({ photo, name, age, size, country, province, city, sex, de
               return l.toUpperCase();
             })}
             <div className="pt-3">
-              <button
-                onClick={handleClick}
-                className="btn bg-yellow-600 text-white border-yellow-700 rounded-md"
-              >
+              <button onClick={handleClick} className="btn bg-yellow-600 text-white border-yellow-700 rounded-md">
                 <h2 className="p-2">Ver mas</h2>
               </button>
             </div>
@@ -33,65 +31,47 @@ function CardAdopcion({ photo, name, age, size, country, province, city, sex, de
         </div>
 
         <div className="card h-96 w-80 bg-secondary ">
-          <div className="grid justify-items-center w-full">
-            <div
-              className="font-bold text-xl p-6 text-fourty cursor-pointer hover:text-white"
-              onClick={handleClick}
-            >
+          <div className="flex flex-col h-full items-center w-full">
+            <div className="font-bold text-xl p-6 text-fourty cursor-pointer hover:text-white" onClick={handleClick}>
               {name.replace(/\b\w/g, function (l) {
                 return l.toUpperCase();
               })}
             </div>
-            <h3 className="p-1 text-white font-bold">
-              Edad: <span className="text-fourty ">{age}</span>
-            </h3>
-            <h3 className="p-1 text-white font-bold">
-              Tamano:{' '}
-              <span className="text-fourty">
-                {size === 'c' ? 'pequeño' : size === 'm' ? 'mediano' : 'grande'}
-              </span>
-            </h3>
-            <h3 className="p-1 text-white font-bold">
-              Sexo{' '}
-              <span className="text-fourty">
-                : {sex === 'h' ? 'hembra' : 'macho'}
-              </span>
-            </h3>
 
-            <h3 className="p-1 text-white font-bold">
-              Pais: <span className="text-fourty">{country}</span>
-            </h3>
-
-            <h3 className="p-1 text-white font-bold">
-              Provincia:<span className="text-fourty"> {province}</span>
-            </h3>
-
-            <h3 className="p-1 text-white font-bold">
-              Descripcion:<span className="text-fourty"> {description}</span>
-            </h3>
-
-            <h3 className="pt-1 pb-4 text-white font-bold">
-              Ciudad: <span className="text-fourty">{city}</span>
-            </h3>
+            <div className="text-center">
+              <h3 className="p-1 text-white font-bold">
+                Edad: <span className="text-fourty ">{age}</span>
+              </h3>
+              <h3 className="p-1 text-white font-bold">
+                Tamano: <span className="text-fourty">{size === "c" ? "pequeño" : size === "m" ? "mediano" : "grande"}</span>
+              </h3>
+              <h3 className="p-1 text-white font-bold">
+                Sexo <span className="text-fourty">: {sex === "h" ? "hembra" : "macho"}</span>
+              </h3>
+              <h3 className="p-1 text-white font-bold">
+                Pais: <span className="text-fourty">{country}</span>
+              </h3>
+              <h3 className="p-1 text-white font-bold">
+                Provincia:<span className="text-fourty"> {province}</span>
+              </h3>
+              <h3 className="pt-1 pb-4 text-white font-bold">
+                Ciudad: <span className="text-fourty">{city}</span>
+              </h3>
+            </div>
 
             {isLogged ? (
-              <div>
-                <button
-                  className="btn bg-yellow-600 text-white border-yellow-700 rounded-md"
-                  onClick={() => setIsOpen(true)}
-                >
-                  <h2 className="p-2">Postulate</h2>
+              <div className="mt-auto mb-4">
+                <button className="shadow-buttonShadow btn-adogtame bg-fourty text-white border-fourtyDark rounded-2xl" onClick={() => setIsOpen(true)}>
+                  <h2 className="p-2 flex items-center">
+                    Ad
+                    <FaPaw />
+                    gtame
+                  </h2>
                 </button>
 
-                <Modal
-                  name={name}
-                  description={description}
-                  open={isOpen}
-                  onClose={() => setIsOpen(false)}
-                ></Modal>
+                <Modal name={name} description={description} open={isOpen} onClose={() => setIsOpen(false)}></Modal>
               </div>
             ) : null}
-            
           </div>
         </div>
       </ReactCardFlip>
