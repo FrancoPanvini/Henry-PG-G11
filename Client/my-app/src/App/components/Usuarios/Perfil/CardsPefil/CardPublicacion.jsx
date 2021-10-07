6f9a8c2b9c89964e46197822e594cffb2d269250
Franco Panvini <francopanvini@gmail.com>
2021-10-07 17:21:42 -0300

    final css fixes CardAdoptions

--- Client/my-app/src/App/components/Usuarios/Perfil/CardsPefil/CardPublicacion.jsx
+++ Client/my-app/src/App/components/Usuarios/Perfil/CardsPefil/CardPublicacion.jsx
@@ -1,37 +1,30 @@
-import React from 'react';
-import { BiTrash, BiEdit } from 'react-icons/bi';
+import React from "react";
+import { BiTrash, BiEdit } from "react-icons/bi";
 import { GrDocumentText } from "react-icons/gr";
 
 function CardPublicacion({ photo, name }) {
-
   return (
-    <div className='grid grid-cols-10 auto-cols-min place-items-center  my-4 rounded-lg py-4 shadow-inner ring ring-primary ring-offset-2  w-full h-1/5 border-4 bg-primary text-white '>
-       <div className="">
-        <img
-          src={photo}
-          alt=''
-          className='w-16 h-16 rounded-full object-cover ring-4 ring-gray-600 ring-opacity-50 transform duration-100 hover:scale-150 hover:pointer '
-        />
-         </div> 
-      <div className="col-span-3">
-        <span className='text-white text-2xl capitalize font-bold'>{name}</span>
+    <div className="grid grid-cols-10 auto-cols-min place-items-center my-4 rounded-lg py-4 shadow-inner ring ring-primary ring-offset-2  w-full h-1/5 border-4 bg-primary text-white ">
+      <div className="">
+        <img src={photo} alt="" className="w-16 h-16 rounded-full object-cover ring-4 ring-gray-600 ring-opacity-50 transform duration-100 hover:scale-125 hover:pointer " />
       </div>
-      <div className='col-span-3'>
-        <span className=' text-gray-200 text-xl mr-8'>
-          En adopcion hace: 20 dias
-        </span>
+      <div className="col-span-3">
+        <span className="text-white text-2xl capitalize font-bold">{name}</span>
       </div>
       <div className="col-span-2">
-        <span className='text-gray-200 text-xl mr-8'>Solicitudes: 15</span>
-        <button className='btn bg-gray-200 text-white py-2 px-2 mr-2 text-2xl rounded-lg shadow-inner font-bold' title="Ver Solicitudes">
-          <GrDocumentText/>
+        <span className=" text-gray-200 text-lg mr-8">En adopcion: 20 dias</span>
+      </div>
+      <div className="col-span-3 flex items-center">
+        <span className="text-gray-200 text-lg mr-8">Solicitudes: 15</span>
+        <button className="btn bg-gray-200 text-white w-10 h-10 flex justify-center items-center mr-2 text-2xl rounded-lg shadow-inner" title="Ver Solicitudes">
+          <GrDocumentText />
         </button>
       </div>
-      <div className="">
-        <button className='btn bg-gray-200 text-green-600 py-2 px-2 text-2xl mr-2 rounded-lg shadow-inner font-bold ' title="Editar publicacion">
+      <div className="flex">
+        <button className="btn bg-gray-200 text-green-600 w-10 h-10 flex justify-center items-center text-2xl mr-2 rounded-lg shadow-inner" title="Editar publicacion">
           <BiEdit />
         </button>
-        <button className='btn bg-gray-200 text-red-600 py-2 px-2 text-2xl mr-2 rounded-lg shadow-inner font-bold' title="Eliminar publicacion">
+        <button className="btn bg-gray-200 text-red-600 w-10 h-10 flex justify-center items-center text-2xl mr-2 rounded-lg shadow-inner" title="Eliminar publicacion">
           <BiTrash />
         </button>
       </div>
