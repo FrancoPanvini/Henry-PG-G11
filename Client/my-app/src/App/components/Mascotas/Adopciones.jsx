11bcff73e28de4fe0ff505a7cb650b065ab081b5
ffranco-a <84511984+ffranco-a@users.noreply.github.com>
2021-10-08 19:07:35 -0300

    merge with develop

--- Client/my-app/src/App/components/Mascotas/Adopciones.jsx
+++ Client/my-app/src/App/components/Mascotas/Adopciones.jsx
@@ -1,26 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
 import CardsContainer from '../Cards/CardsContainer';
 import FiltersBar from '../FilterBar/FiltersBar';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
+import FormularioPosteo from './FormularioPosteo';
 
 function Adopciones() {
   const isLogged = useSelector((state) => state.isLogged);
+  const [isOpen, setIsOpen] = useState(false);
 
   return (
     <div>
-      <div className="grid grid-cols-7 place-items-center bg-gray-200">
-        <FiltersBar className="place-self-center fixed" />
-        <CardsContainer title="ADOPCIONES" className="col-span-6" />
+      <div className='grid grid-cols-7 place-items-center bg-gray-200'>
+        <FiltersBar className='place-self-center fixed' />
+        <CardsContainer title='ADOPCIONES' className='col-span-6' />
       </div>
 
       {isLogged && (
-        <div className="flex justify-center py-12 bg-gray-200">
-          <Link to="/adopciones/ofrecer" className="mx-auto">
-            <button className="btn btn-lg bg-primary text-white">
-              Ofrecer una mascota en adopción
-            </button>
-          </Link>
+        <div className='flex justify-center py-12 bg-gray-200'>
+          <button className='btn btn-lg bg-primary text-white' onClick={() => setIsOpen(true)}>Ofrecer una mascota en adopción</button>
+          {isOpen && <FormularioPosteo onClose={() => setIsOpen(false)} />}
         </div>
       )}
     </div>
