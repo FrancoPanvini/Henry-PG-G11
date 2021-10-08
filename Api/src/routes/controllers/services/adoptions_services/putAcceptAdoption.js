afac94ee145c82d68446b3f819107a146e254ab5
ffranco-a <84511984+ffranco-a@users.noreply.github.com>
2021-10-08 17:49:29 -0300

    comment cleaning

--- Api/src/routes/controllers/services/adoptions_services/putAcceptAdoption.js
+++ Api/src/routes/controllers/services/adoptions_services/putAcceptAdoption.js
@@ -3,14 +3,9 @@ const { Adoptions, Pets } = require("../../../../db");
 const putAcceptAdoption = async (req, res) => {
   const { id } = req.params;
 
-  //* Change state in adoption process to "a" (accepted)
+  //* Change state in adoption process to "a" (accepted), then set Pet adopted state as `true` and add its Adopterid
   let process;
   try {
-    // Fran, esta forma que hicimos no impedía dos cosas: 1. que se vuelva a mandar la aceptación del proceso, y 2. que un proceso cerrado pase a estar aceptado (cuando en realidad solo los procesos pendientes deberían poder aceptarse)... además agregué lo que nos quedó pendiente: que se cierren todos los otros procesos pendientes con la misma mascota que se acaba de adoptar
-    // await Adoptions.update({ state: "a" }, { where: { id: parseInt(id) } });
-    // acceptedProcess = await Adoptions.findOne({ where: { id: parseInt(id) } });
-    // if (!acceptedProcess) throw new Error(`This application process does not exist in our database`);
-
     //* Find process and check wether it is pending or not
     process = await Adoptions.findOne({ where: { id: parseInt(id) } });
     if (!process) throw new Error(`This application process does not exist in our database`);
