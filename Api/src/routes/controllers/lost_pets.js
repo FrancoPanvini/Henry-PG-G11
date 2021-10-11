d4b0c11814b291265271d7344060545d670b4ccd
LucasChaves12 <84579422+LucasChaves12@users.noreply.github.com>
2021-10-11 19:20:09 -0300

    feat route lostPetDetail

--- Api/src/routes/controllers/lost_pets.js
+++ Api/src/routes/controllers/lost_pets.js
@@ -3,11 +3,13 @@ const router = express.Router();
 const getLostPets = require("./services/lost_pets_services/getLostPets");
 const postLostPet = require("./services/lost_pets_services/postLostPet");
 const {deleteLostPet} = require("./services/lost_pets_services/deleteLostPet");
+const getLostPetDetail = require("./services/lost_pets_services/getLostPetDetail");
 
 //ver todos los animales perdidos
 router.get("/", getLostPets);
 
 //Ver detalle de una mascota
+router.get("/:id", getLostPetDetail)
 
 //Publicar una nueva mascota
 router.post("/", postLostPet);
