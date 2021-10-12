586ec77f3d9826b4bf5856de529529feb7e60ae7
ffranco-a <84511984+ffranco-a@users.noreply.github.com>
2021-10-12 12:49:07 -0300

    links activos

--- Client/my-app/src/App/components/Refugios/Refugios.jsx
+++ Client/my-app/src/App/components/Refugios/Refugios.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-// import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 //? Components
 import CardsContainer from '../Cards/CardsContainer';
@@ -9,9 +9,9 @@ function Refugios() {
   return (
     <div className='grid grid-cols-7 place-items-center bg-gray-200 relative'>
       <button className='btn btn-lg bg-attention text-white border-primaryDark absolute top-18 2xl:left-12 lg:left-8'>
-        {/* <Link to='/refugios/map'> */} 
+        <Link to='/refugios/map'>
           Buscar por mapa
-        {/* </Link> */}
+        </Link>
       </button>
       <FiltersBar className='place-self-center' />
       <CardsContainer title='REFUGIOS' className='col-span-6' />
