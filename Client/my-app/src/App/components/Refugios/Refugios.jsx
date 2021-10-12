c293833e2b68c6017cdbe6c7927c64089b5d482c
ffranco-a <84511984+ffranco-a@users.noreply.github.com>
2021-10-12 12:20:57 -0300

    botones mapas listos (descomentar Links para que funcionen en PErdidos y Refugios)

--- Client/my-app/src/App/components/Refugios/Refugios.jsx
+++ Client/my-app/src/App/components/Refugios/Refugios.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
+// import { Link } from 'react-router-dom';
+
+//? Components
 import CardsContainer from '../Cards/CardsContainer';
 import FiltersBar from '../FilterBar/FiltersBar';
 
 function Refugios() {
   return (
-    <div className="grid grid-cols-7 place-items-center bg-gray-200">
-      <FiltersBar className="place-self-center" />
-      <CardsContainer title="REFUGIOS" className="col-span-6" />
+    <div className='grid grid-cols-7 place-items-center bg-gray-200 relative'>
+      <button className='btn btn-lg bg-attention text-white border-primaryDark absolute top-18 2xl:left-12 lg:left-8'>
+        {/* <Link to='/refugios/map'> */} 
+          Buscar por mapa
+        {/* </Link> */}
+      </button>
+      <FiltersBar className='place-self-center' />
+      <CardsContainer title='REFUGIOS' className='col-span-6' />
     </div>
   );
 }
