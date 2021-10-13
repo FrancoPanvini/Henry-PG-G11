c84b118bd76afa63ffccacc73fc2bb98356054e4
Franco Panvini <francopanvini@gmail.com>
2021-10-13 15:32:13 -0300

    fix-bug selectUbication value

--- Client/my-app/src/App/components/FilterBar/SelectCountries/SelectUbication.jsx
+++ Client/my-app/src/App/components/FilterBar/SelectCountries/SelectUbication.jsx
@@ -11,64 +11,49 @@ function SelectUbication({ urlFilter, handleSetUrl }) {
     dispatch(getCities());
   }, [dispatch]);
 
-  const pais = useSelector((state) => state.countries);
-  const provincia = useSelector((state) => state.provinces);
-  const ciudad = useSelector((state) => state.cities);
+  const pais = useSelector(state => state.countries);
+  const provincia = useSelector(state => state.provinces);
+  const ciudad = useSelector(state => state.cities);
   const [countryId, setCountryId] = useState(null);
   const [provinceId, setProvinceId] = useState(null);
 
-  const handleFilterUbication = (e) => {
+  const handleFilterUbication = e => {
     handleSetUrl(e);
 
-    e.target.name === 'country'
-      ? setCountryId(e.target.value)
-      : setProvinceId(e.target.value);
+    e.target.name === 'country' ? setCountryId(e.target.value) : setProvinceId(e.target.value);
   };
 
   return (
     <div className='p-1 mb-2 flex flex-wrap justify-between items-center '>
       <label className='w-1/2 pr-8  font-bold'>Ubicación</label>
-      <select
-        onChange={handleFilterUbication}
-        name='country'
-        defaultValue='default'
-        value={urlFilter.country}
-        /* id="paises" */ className='w-full my-2 rounded-md px-1'>
+      <select onChange={handleFilterUbication} name='country' defaultValue='default' /* id="paises" */ className='w-full my-2 rounded-md px-1'>
         <option value='default'>Seleccionar</option>
         {pais &&
-          pais.map((e) => (
+          pais.map(e => (
             <option key={e.id} name={e.id} value={e.id}>
               {e.name}
             </option>
           ))}
       </select>
       <br />
-      <select
-        onChange={handleFilterUbication}
-        className='w-full rounded-md px-1 my-2' /* id="provincias" */
-        name='province'
-        value={urlFilter.province}>
+      <select onChange={handleFilterUbication} className='w-full rounded-md px-1 my-2' /* id="provincias" */ name='province'>
         <option>Seleccionar</option>
         {provincia &&
           provincia
-            .filter((p) => parseInt(p.CountryId) === parseInt(countryId))
-            .map((e) => (
+            .filter(p => parseInt(p.CountryId) === parseInt(countryId))
+            .map(e => (
               <option key={e.id} value={e.id} name='city' onClick={e.target}>
                 {e.name}
               </option>
             ))}
       </select>
       <br />
-      <select
-        onChange={handleSetUrl}
-        className='w-full rounded-md px-1 my-2' /* id="provincias" */
-        name='city'
-        value={urlFilter.city}>
+      <select onChange={handleSetUrl} className='w-full rounded-md px-1 my-2' /* id="provincias" */ name='city'>
         <option>Seleccionar</option>
         {ciudad &&
           ciudad
-            .filter((p) => parseInt(p.ProvinceId) === parseInt(provinceId))
-            .map((e) => (
+            .filter(p => parseInt(p.ProvinceId) === parseInt(provinceId))
+            .map(e => (
               <option key={e.id} value={e.id}>
                 {e.name}
               </option>
