52da811d95027da31fd84cf14fb7fed99ebb5a9f
ffranco-a <84511984+ffranco-a@users.noreply.github.com>
2021-10-20 16:07:17 -0300

    profile photo upload en perfil refugio

--- Client/my-app/src/App/components/Usuarios/Perfil/CardsPefil/PopUpPhoto.jsx
+++ Client/my-app/src/App/components/Usuarios/Perfil/CardsPefil/PopUpPhoto.jsx
@@ -1,32 +1,51 @@
-import React from "react";
-import ReactDom from "react-dom";
-import { FaSave } from "react-icons/fa";
-import { IoIosCloseCircle } from "react-icons/io";
-import UploadImage from "../../../cargue-fotos/UploadImage";
+import React, { useState } from 'react';
+import ReactDom from 'react-dom';
+import { FaSave } from 'react-icons/fa';
+import { IoIosCloseCircle } from 'react-icons/io';
+import UploadImage from '../../../cargue-fotos/UploadImage';
 
-const PopUpPhoto = ({ onClose, url, setUrl }) => {
+const PopUpPhoto = ({ onClose, setUrl }) => {
+  const [newUrl, setNewUrl] = useState([]);
+
+  const handleDisabled = () => {
+    if (newUrl.length === 1) {
+      return false;
+    }
+
+    return true;
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setUrl(newUrl);
+    onClose();
+  };
+
   return ReactDom.createPortal(
     <>
-      <div className="fixed inset-0 bg-gray-50 bg-opacity-70 z-40">
-        <div className="fixed inset-0 z-50 overflow-y-scroll ">
-          <div className="panel relative top-10 bg-primary mx-auto w-2/4 max-w-3xl">
-            <div>
-              <IoIosCloseCircle
-                title="Cancelar y volver"
-                className="text-thirty absolute top-1 right-1 text-3xl hover:text-thirtyLight cursor-pointer transition-all z-50"
-                onClick={onClose}
-              />
-            </div>
-            <label>Foto: (incluir una única foto)</label>
-            <UploadImage url={url} setUrl={setUrl} />
-            <button className="btn bg-green-600 text-white w-10 h-10 flex shadow-2xl justify-center items-center text-xl rounded-full mx-auto">
-              <FaSave onClick={onClose} title="Guardar" />
+      <div className='fixed inset-0 bg-gray-50 bg-opacity-70 z-40'>
+        <div className='fixed inset-0 z-50 overflow-y-scroll '>
+          <div className='panel relative top-10 bg-primary mx-auto w-2/4 max-w-3xl'>
+            <IoIosCloseCircle
+              title='Cancelar y volver'
+              className='text-thirty absolute top-1 right-1 text-3xl hover:text-thirtyLight cursor-pointer transition-all z-50'
+              onClick={onClose}
+            />
+            <label>Foto: (sólo una foto)</label>
+            <UploadImage url={newUrl} setUrl={setNewUrl} />
+            <button
+              disabled={handleDisabled()}
+              onClick={handleSubmit}
+              className={`${
+                handleDisabled() ? 'opacity-50 cursor-default border-b-2 border-transparent' : 'btn'
+              } bg-green-600 text-white w-12 h-12 flex shadow-2xl justify-center items-center text-xl rounded-full mx-auto`}>
+              <FaSave onClick={onClose} title='Guardar' />
             </button>
           </div>
         </div>
       </div>
     </>,
-    document.getElementById("portal")
+    document.getElementById('portal')
   );
 };
 
