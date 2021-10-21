8a8937b96de1652db6cf6a76a47102f870285122
ffranco-a <84511984+ffranco-a@users.noreply.github.com>
2021-10-20 21:37:02 -0300

    nosotros on the way

--- Client/my-app/src/App/components/info/Nosotros.jsx
+++ Client/my-app/src/App/components/info/Nosotros.jsx
@@ -1,9 +1,93 @@
 import React from 'react';
+import NosotrosCard from './NosotrosCard';
+import fotoDavid from '../../../images/DavidMendozaLopez.png';
+import fotoAparicio from '../../../images/FrancoAparicio.jpg';
+import fotoPanvini from '../../../images/FrancoPanvini.jpg';
+import fotoPetri from '../../../images/SantiagoPetri.jpg';
+import fotoLucas from '../../../images/LucasChaves.jpg';
+import fotoDamian from '../../../images/DamianDiego.png';
 
 function Nosotros() {
   return (
-    <div>
-      SOY NOSOTROS
+    <div className='h-screen82 px-16 grid grid-cols-3 grid-rows-2 gap-4 bg-gradient-to-r from-gray-300 to-thirtyLight'>
+      <NosotrosCard 
+        photo={fotoLucas}
+        name='Lucas Chaves'
+        alias='Luquitas'
+        // description
+        strengths='Back-End'
+        mail='chaves.lucas2000@gmail.com'
+        linkedin='https://www.linkedin.com/in/lucas-chaves-dev/'
+        github='https://github.com/LucasChaves12'
+        // twitter
+        // portfolio
+        extraCSS
+      />
+      <NosotrosCard 
+        photo={fotoDamian}
+        name='Damian Diego'
+        alias='Dami'
+        // description
+        strengths='Front-End'
+        mail='damian.diego.2510@gmail.com'
+        linkedin='https://www.linkedin.com/in/damianldiego/'
+        github='https://github.com/damldieg'
+        twitter='https://twitter.com/DamianLDiego'
+        portfolio='https://damdiegdevfolio.netlify.app'
+        extraCSS
+      />
+      <NosotrosCard 
+        photo={fotoDavid}
+        name='David Mendoza Lopez'
+        alias='Deivid'
+        // description
+        strengths='Front-End'
+        mail='davidmenlop@gmail.com'
+        linkedin='https://www.linkedin.com/in/davidmenlop/'
+        github='https://github.com/davidmenlop'
+        twitter='https://twitter.com/DavidmenWeb'
+        portfolio='https://portafolio-ivory.vercel.app'
+        extraCSS='object-top'
+      />
+      <NosotrosCard 
+        photo={fotoAparicio}
+        name='Franco Aparicio'
+        alias='Fran'
+        description='"El bug está en los detalles"'
+        strengths='Front-End'
+        mail='ffrancoaparicio@gmail.com'
+        linkedin='https://www.linkedin.com/in/franco-aparicio'
+        github='https://github.com/ffranco-a'
+        // twitter
+        // portfolio
+        extraCSS='object-top'
+      />
+      <NosotrosCard 
+        photo={fotoPetri}
+        name='Santiago Petri'
+        alias='Petri'
+        // description
+        strengths='Back-End'
+        mail='santiagopetri@hotmail.com'
+        linkedin='https://www.linkedin.com/in/santiagopetri/'
+        github='https://github.com/DonPepo'
+        // twitter
+        // portfolio
+        extraCSS
+      />
+      <NosotrosCard 
+        photo={fotoPanvini}
+        name='Franco Panvini'
+        alias='Panvini'
+        // description
+        strengths='Back-End'
+        mail='francopanvini@gmail.com'
+        linkedin='https://linkedin.com/in/franco-panvini'
+        github='https://github.com/FrancoPanvini'
+        // twitter
+        // portfolio
+        extraCSS
+      />
     </div>
   )
 }
---
+++ Client/my-app/src/App/components/info/NosotrosCard.jsx
@@ -0,0 +1,72 @@
+import React, { useState } from 'react';
+import { FaGithubSquare, FaLinkedin, FaTwitterSquare } from 'react-icons/fa';
+
+function NosotrosCard({ photo, name, alias, description, strengths, mail, linkedin, github, twitter, portfolio, extraCSS, }) {
+  const [showTooltip, setShowTooltip] = useState(false);
+
+  const timer = () => {
+    setShowTooltip(true);
+    setTimeout(() => setShowTooltip(false), 1500);
+  };
+
+  const copyText = (e) => {
+    console.log(e.target);
+    console.log(e.target.innerText);
+    console.log(e.target.value);
+    navigator.clipboard.writeText(e.target.innerText);
+    timer();
+  };
+
+  return (
+    <div className='m-8 relative'>
+      <div className='mt-12 flex floorTinyShadowCircle'>
+        <div className='z-40 w-5/12 h-full flex items-center'>
+          <div className='rounded-full w-full aspect-h-1 aspect-w-1'>
+            <img src={photo} alt={`Foto de ${name}`} className={`h-full w-full rounded-full object-cover shadow-similBorderWhite ${extraCSS}`} />
+          </div>
+        </div>
+        <div className='relative ml-2 z-40 w-7/12 flex flex-col items-start justify-between text-fourtyDark'>
+          <div className='text-2xl font-bold'>{name}</div>
+          <div className='mb-auto text-sm'>alias {alias}</div>
+          <div className='text-center italic'>{description}</div>
+          <div className='mt-auto'>Fortalezas: {strengths}</div>
+          {portfolio && (
+            <div className='hover:underline'>
+              <a href={portfolio} target='_blank' rel='noreferrer'>
+                Click aquí para ver mi portfolio.
+              </a>
+            </div>
+          )}
+          <div
+            onClick={copyText}
+            value={mail}
+            title='¡Click para copiar este mail!'
+            className='text-center mb-2 cursor-pointer hover:underline relative'>
+            {mail}
+          </div>
+          <div
+            className={`${
+              showTooltip ? 'opacity-100' : 'opacity-0 invisible'
+            } z-50 absolute bottom-0 2xl:right-8 xl:-right-4 lg:-right-12 text-sm text-gray-300 bg-fourtyDark rounded-full py-1 px-3 min-w-max text-center transition-all`}>
+            ¡Correo copiado en el portapapeles!
+          </div>
+          <div>
+            <a href={linkedin} target='_blank' rel='noreferrer'>
+              <FaLinkedin className='inline text-3xl 2xl:mr-8 xl:mr-4' />
+            </a>
+            <a href={github} target='_blank' rel='noreferrer'>
+              <FaGithubSquare className='inline text-3xl 2xl:mr-8 xl:mr-4' />
+            </a>
+            {twitter && (
+              <a href={twitter} target='_blank' rel='noreferrer'>
+                <FaTwitterSquare className='inline text-3xl 2xl:mr-8 xl:mr-4' />
+              </a>
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default NosotrosCard;
